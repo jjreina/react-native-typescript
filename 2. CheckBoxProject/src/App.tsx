52f468e357a1f7a2160686e0c3f88a3fd420007c
jjreina <jreinapascual@gmail.com>
2018-03-21 19:31:16 +0100

    React Native CheckBoxProject - Added diferents kind of checkbox.

--- 2. CheckBoxProject/src/App.tsx
+++ 2. CheckBoxProject/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { CheckBox } from 'react-native-elements'
+import { View, TouchableHighlight } from 'react-native'
 
 interface State {
   checked: boolean
+  checkedLastIcon: boolean
 }
 
 interface Props {}
@@ -12,24 +14,62 @@ export default class App extends React.Component<Props, State> {
   constructor(props) {
     super(props)
     this.state = {
-      checked: false
+      checked: false,
+      checkedLastIcon: false
     }
   }
 
   public render() {
     return (
-      <CheckBox
-        center
-        title='Click Here'
-        checked={this.state.checked}
-        containerStyle={{marginTop: 30}}
-        onPress={this.onPress}
-      />
+      <View>
+        <CheckBox
+          center
+          title='Click Here'
+          checked={this.state.checked}
+          containerStyle={{marginTop: 30}}
+          onPress={this.onPressFirstIcon}
+        />
+        <CheckBox
+          center
+          title='Click Here'
+          checkedIcon='dot-circle-o'
+          uncheckedIcon='circle-o'
+          checked={this.state.checked}
+          checkedColor='blue'
+          component={TouchableHighlight}
+        />
+        <CheckBox
+          center
+          title='Click Here to Remove This Item'
+          iconRight
+          iconType='material'
+          checkedIcon='clear'
+          uncheckedIcon='add'
+          checkedColor='red'
+          checked={this.state.checked}
+          component={TouchableHighlight}
+        />
+        <CheckBox
+          center
+          title='Click Here to Remove This Item'
+          iconRight
+          iconType='material'
+          checkedIcon='done'
+          uncheckedIcon='done'
+          checkedColor='purple'
+          checked={this.state.checkedLastIcon}
+          onPress={this.onPressLastIcon}
+        />
+      </View>
     )
   }
 
-  private onPress = () => {
+  private onPressFirstIcon = () => {
     this.setState({ checked: !this.state.checked})
+  }
+
+  private onPressLastIcon = () => {
+    this.setState({ checkedLastIcon: !this.state.checkedLastIcon})
   }
 
 }
